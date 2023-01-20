86f4eacca16d4af3239f1262de43a9cef2eae6f9
Dzhaparov Bekmamat Kanybekovich <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-01-20 20:00:05 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(GoForAWalk(30, 24));
+        System.out.println(GoForAWalk(20, 12));
+        System.out.println(GoForAWalk(46, 33));
+        System.out.println(GoForAWalk(21, 24));
+        System.out.println(GoForAWalk(60, -12));
+    }
+    public static String GoForAWalk(int age, int temperature) {
+        if ((age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) ||
+                (age < 20 && temperature >= 0 && temperature <= 28) ||
+                (age > 45 && temperature >= -10 && temperature <= 25)) {
+            return "Можно идти гулять, ура !";
+        } else {
+            return "Оставайтесь дома, пожалуйста";
+        }
+
+
+    }
+}
