3bf329f133c67fed4408d5d02674f1a1e8da6158
Dzhaparov Bekmamat Kanybekovich <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-01-23 14:41:56 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        System.out.println(GoForAWalk(30, 24));
-        System.out.println(GoForAWalk(20, 12));
-        System.out.println(GoForAWalk(46, 33));
-        System.out.println(GoForAWalk(21, 24));
-        System.out.println(GoForAWalk(60, -12));
+        System.out.println(goForAWalk(30, 24));
+        System.out.println(goForAWalk(20, 12));
+        System.out.println(goForAWalk(46, 33));
+        System.out.println(goForAWalk(21, 24));
+        System.out.println(goForAWalk(60, -12));
     }
-    public static String GoForAWalk(int age, int temperature) {
+    public static String goForAWalk(int age, int temperature) {
         if ((age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) ||
                 (age < 20 && temperature >= 0 && temperature <= 28) ||
                 (age > 45 && temperature >= -10 && temperature <= 25)) {
